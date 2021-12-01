54014a2b1adfc8fefcf3f4061e6a7472a4f753f6
Nathan Wong <nathan@nathanwong.co.uk>
2021-12-01 22:44:38 +0000

    Day 1 puzzle solutions

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,39 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val numbers = input.map { it.toInt() }
+
+        var count = 0
+        for (i in numbers.indices) {
+            if (i > 0 && numbers[i] > numbers[i-1]) {
+                count += 1
+            }
+        }
+
+        return count
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val numbers = input.map { it.toInt() }
+        val windowSize = 3
+
+        var count = 0
+
+        for (i in windowSize..numbers.lastIndex) {
+            val previousWindow = numbers[i-1] + numbers[i-2] + numbers[i-3]
+            val currentWindow = numbers[i] + numbers[i-1] + numbers[i-2]
+
+            if (currentWindow > previousWindow) {
+                count +=1
+            }
+        }
+
+        return count
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
